1881d6693e38be14e3c6d081eb0ea42d91470d83
Satyateja Pothuru <satyateja27@gmail.com>
2017-04-15 20:18:46 -0700

    Add access methods in relationship

--- src/RelationShip.java
+++ src/RelationShip.java
@@ -13,5 +13,43 @@ public class Relationship {
         this.type = type;
     }
 
-    
+    public ClassOrInterfaceDeclaration getA() {
+        return a;
+    }
+
+    public void setA(ClassOrInterfaceDeclaration a) {
+        this.a = a;
+    }
+
+    public String getMultiplicityA() {
+        return multiplicityA;
+    }
+
+    public void setMultiplicityA(String multiplicityA) {
+        this.multiplicityA = multiplicityA;
+    }
+
+    public ClassOrInterfaceDeclaration getB() {
+        return b;
+    }
+
+    public void setB(ClassOrInterfaceDeclaration b) {
+        this.b = b;
+    }
+
+    public String getMultiplicityB() {
+        return multiplicityB;
+    }
+
+    public void setMultiplicityB(String multiplicityB) {
+        this.multiplicityB = multiplicityB;
+    }
+
+    public UmlRelationShipType getType() {
+        return type;
+    }
+
+    public void setType(UmlRelationShipType type) {
+        this.type = type;
+    }
 }
